7ef96d6a01a9f09188fa243833dc318fa8f18553
TotiManuel <manuel.mandaio@gmail.com>
2022-06-28 11:14:18 -0300

    Commit Automatico

--- GesionBomberos.py
+++ GesionBomberos.py
@@ -2,27 +2,37 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 
-#Definir acciones menu
-def Accion1():Programa.destroy
-def AccionA():texto.configure(text="toti")
-#Fin acciones menu
+def accionUno():
+        Programa.destroy
 
 Programa=Tk()
-Programa.geometry("1000x1000")
-ventana=ttk.Frame(Programa, padding=10)
+Programa.geometry("500x500")
+Programa.title("Bomberos Voluntarios Villa Nueva")
+ventana=ttk.Frame(Programa, padding=0)
 ventana.grid()
 
-#ventana1=tk.Tk()
-#ventana1.title("Toti")
+#Colocacion del Menu en la interfaz
+barra_menus=tk.Menu(Programa)
+Programa.config(menu=barra_menus)
 
-texto=tk.Label(ventana,text="texto")
-texto.place(x=200,y=200)
+#Menus Definidos
+menu=tk.Menu(barra_menus, tearoff=False)
+menuArchivo=tk.Menu(barra_menus, tearoff=False)
+#SubMenus Definidos
+submenu=tk.Menu(menu,tearoff=False)
 
+#Opciones del Menu
+barra_menus.add_cascade(label="Menu", menu=menu)
+barra_menus.add_cascade(label="Archivo", menu=menuArchivo)
 
-ttk.Label(ventana, text="Hello World").grid(column=0, row=0)
-ttk.Button(ventana, text="Quit", command=Programa.destroy).grid(column=1,row=0)
+#Opciones SubMenus Principal 
+menu.add_command(label="Opcion 1", command=Programa.destroy)
+menuArchivo.add_command(label="Opcion 2", command=Programa.destroy)
 
+#Opciones SubMenus Secundario
+menu.add_cascade(label="SubMenu1",menu=submenu)
 
+#Opciones SubMenus Terciario
+submenu.add_command(label="SubMenu 2", command=Programa.destroy)
 
 Programa.mainloop()
-
